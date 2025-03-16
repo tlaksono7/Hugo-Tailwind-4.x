8ee7bb5dc7bb57df9b071dfa07e4a12cc2cff1e8
4044ever <minidisco@gmx.de>
2025-03-16 13:23:26 +0800

    Updated for TailwindCSS v4.*
    Updated for TailwindCSS v4.*

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,7 @@
-module.exports = {
-  content: ['./layouts/**/*.html', './content/**/*.md'],
-  theme: {
-    extend: {
-        typography: {
-            DEFAULT: {
-                css: {
-                    "code::before": {content: ''},
-                    "code::after": {content: ''}
-                }
-            }
-        }
-    }
-},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+/*
+This file is present to satisfy a requirement of the Tailwind CSS IntelliSense
+extension for Visual Studio Code.
+
+https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
+
+*/
